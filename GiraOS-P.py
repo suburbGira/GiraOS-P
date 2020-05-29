2a41f463d001715053b8d0a2e33ec993b835eb9e
suburbGira <58662333+suburbGira@users.noreply.github.com>
2020-05-29 22:12:35 +0100

    update to 0.4

--- GiraOS-P.py
+++ GiraOS-P.py
@@ -1,19 +1,38 @@
-#GGGGGGGGGGG IIIIIIIII RRRRRRRR AA                                       22222   00000   11   77777
-#G               I     R      R A A        sssss   ooooo   ffff    t    2     2 0    00 1 1       7
-#G               I     R      R A  A      s     s o     o f    f ttttt       2  0   0 0   1      7
-#G               I     RRRRRRRR A   A     s       o     o f        t        2   0  0  0   1     7
-#G   GGGGGGG     I     R R      AA   A     sssss  o     o ffff     t       2    0  0  0   1    7
-#G         G     I     R  R     A AA  A         s o     o f        t      2     0 0   0   1   7
-#G         G     I     R   R    A   AA A  s     s o     o f        t     2      00    0   1   7
-#GGGGGGGGGGG IIIIIIIII R    R   A     AAA  sssss   ooooo  f        t    2222222  00000  11111 7     DO NOT SELL THIS SOFTWARE
+copyright="GIRAsoft 2017/2018." # copy protection
+prodnum="GPN-PY-BE-GOSP04" # internal number for software cataloguing
+key=12341234 # product key - antipiracy
+devidsub=1963 # developer id list here - makes sure only devs can access beta
+deviddark=2729
+bd="3 Feb 2018 @ 18:51 GMT" # build date for reference
+#GGGGGGGGGGG IIIIIIIII RRRRRRRR AA                                       22222   00000   11   77777        22222   00000   11    88888
+#G               I     R      R A A        sssss   ooooo   ffff    t    2     2 0    00 1 1       7       2     2 0    00 1 1   8     8
+#G               I     R      R A  A      s     s o     o f    f ttttt       2  0   0 0   1      7             2  0   0 0   1   8     8
+#G               I     RRRRRRRR A   A     s       o     o f        t        2   0  0  0   1     7             2   0  0  0   1   8     8
+#G   GGGGGGG     I     R   R    AA   A     sssss  o     o ffff     t       2    0  0  0   1    7    -----    2    0  0  0   1    88888
+#G         G     I     R    R   A AA  A         s o     o f        t      2     0 0   0   1   7             2     0 0   0   1   8     8
+#G         G     I     R     R  A   AA A  s     s o     o f        t     2      00    0   1   7            2      00    0   1   8     8
+#GGGGGGGGGGG IIIIIIIII R      R A     AAA  sssss   ooooo  f        t    2222222  00000  11111 7           2222222  00000  11111  88888   DO NOT SELL THIS SOFTWARE
 import sys # enable exiting from within
-print("GiraOS/P ver 0.3 -- copyright GIRAsoft 2017. You should have recieved a copy of the GNU GPL with this software. If not, you can find it on the Internet, or in most Linux software.")
-print("DO NOT ATTEMPT TO SELL THIS SOFTWARE. IF YOU HAVE PAID FOR THIS SOFTWARE, PLEASE CONTACT greatgiratheepic@gmail.com!")
-print("NOTE: Due to Python being weird, please use ONLY numbers as input.")
+print "GiraOS/P ver 0.4 -- copyright", copyright," You should have recieved a copy of the GNU GPL with this software. If not, you can find it on the Internet, or in most Linux software." # print the copyright and gpl notice
+print("DO NOT ATTEMPT TO SELL THIS SOFTWARE. IF YOU HAVE PAID FOR THIS SOFTWARE, PLEASE CONTACT greatgiratheepic@gmail.com!") # this was written in linux by one person - it should be free
+print("NOTE: Due to Python being weird, please use ONLY numbers as input.") # i don't know how to fix that, contact me if you do
+checkkey=input("Enter your product key. >") # make sure this is legal
+if checkkey != key:
+	print("Error 01: Invalid key") # o noez, we're illegal
+	sys.exit("exit::invalid key")
+else:
+	checkid=input("Enter your developer ID. >") # devs only
+	if checkid != devidsub:
+		if checkid != deviddark:
+			print("Error 02: Invalid developer ID")
+			sys.exit("exit::invalid developer ID")
 while True:
-	command = input("Press 1 for test, 2 for verboseness, 3 to print the changelog or 10 to exit: ")
+	if copyright != "GIRAsoft 2017/2018.": # make sure we're good
+		print("Error 03: Invalid Copyright message")
+		sys.exit("exit::invalid copyright")
+	command = input("Enter 1 for test, 2 for verboseness, 3 to print the changelog or 10 to exit: ") # hooray for commands
 	if command == 1:
-		print("Testing")
+		print("Testing") # app name: test
 		print("esting")
 		print("sting")
 		print("ting")
@@ -22,40 +41,48 @@ while True:
 		print("g")
 		print("done executing")
 	elif command == 2:
-		print("Expression of surprise or astonishment at the unintended performance of an undefined action!") # Oops!
-		print("I regret that the system which processes input as a series of ones and zeroes known as binary belonging to the organism known as a human who has been given the designation of me has performed an action which was not intended to be performed by its manufacturers!") # My system crashed!
-		print("Additionally, the aforementioned problem has caused it to magnetise small spinning discs inside it in such a way that any data stored on them has been removed!") # I lost my deeta!
-		print("However, to combat this from happening, I possessed a computer program which is designed to prevent this type of error from happening by stopping malicious data from editing the performance of my device which processes input as a series of ones and zeros known as binary!") # But I had an antivirus!
-		print("I regret to inform you that the aforementioned computer program possessed by you is currently insufficient to prevent the loss of data due to the unintended magnetisation and/or demagnetisation of small spinning discs inside the system which processes input as a series of ones and zeroes known as binary belonging to the organism known as a human who has been given the designation of you!") # Antivirus is not enough!
-		print("The organism known as a human who has been given the designation of you possess the requirement to purchase a computer program which has ben given the name of Protegent 360 Security by its creators, Unistal Global Inc. which is designed to prevent malicious computer programs from entering your system which processes input as a series of ones and zeroes known as binary!") # You need Protegent!
-		print("The only computer software currently existing in the planet we are standing on which possesses the power to prevent the unintended magnetisation and/or demagnetisation of small spinning discs inside a system which processes input as a series of ones and zeroes known as binary in such a way that any data stored on them is lost!") # World's only antivirus with deeta recovery software!
-		print("Use the organ known as a brain to perform the action of thinking beyond computer programs which prevent malicious computer programs entering systems which process input as a series of ones and zeros known as binary!") # Think beyond antivirus...
-		print("Use the organ known as a brain to perform the action of thinking about a computer program which has ben given the name of Protegent 360 Security by its creators, Unistal Global Inc. which is designed to prevent malicious computer programs from entering your system which processes input as a series of ones and zeroes known as binary!") # Think Protegent.
+		print("Expression of surprise or astonishment at the unintended performance of an undefined action!") # app name: verbose
+		print("I regret that the system which processes input as a series of ones and zeroes known as binary belonging to the organism known as a human who has been given the designation of me has performed an action which was not intended to be performed by its manufacturers!")
+		print("Additionally, the aforementioned problem has caused it to magnetise small spinning discs inside it in such a way that any data stored on them has been removed!")
+		print("However, to combat this from happening, I possessed a computer program which is designed to prevent this type of error from happening by stopping malicious data from editing the performance of my device which processes input as a series of ones and zeros known as binary!")
+		print("I regret to inform you that the aforementioned computer program possessed by you is currently insufficient to prevent the loss of data due to the unintended magnetisation and/or demagnetisation of small spinning discs inside the system which processes input as a series of ones and zeroes known as binary belonging to the organism known as a human who has been given the designation of you!")
+		print("The organism known as a human who has been given the designation of you possess the requirement to purchase a computer program which has ben given the name of Protegent 360 Security by its creators, Unistal Global Inc. which is designed to prevent malicious computer programs from entering your system which processes input as a series of ones and zeroes known as binary!")
+		print("The only computer software currently existing in the planet we are standing on which possesses the power to prevent the unintended magnetisation and/or demagnetisation of small spinning discs inside a system which processes input as a series of ones and zeroes known as binary in such a way that any data stored on them is lost!")
+		print("Use the organ known as a brain to perform the action of thinking beyond computer programs which prevent malicious computer programs entering systems which process input as a series of ones and zeros known as binary!")
+		print("Use the organ known as a brain to perform the action of thinking about a computer program which has ben given the name of Protegent 360 Security by its creators, Unistal Global Inc. which is designed to prevent malicious computer programs from entering your system which processes input as a series of ones and zeroes known as binary!")
 	elif command == 3:
-		print("     Ver 0.3      ")
-		print("Added logo in code")
+		print("     Ver 0.4      ") # app name: changelog
+		print(" Added copyright  ")
+		print("Added product num.")
 		print("==================")
 		ask=input("Show more? 1:Yes 2:No >")
 		if ask==1:
-			print("     Ver 0.2     ")
-			print("Added Verboseness")
-			print("and rewrote comm-")
-			print("   and system.   ")
-			print("=================")
+			print("     Ver 0.3      ")
+			print("Added logo in code")
+			print("==================")
 			ask=input("Show more? 1:Yes 2:No >")
 			if ask==1:
-				print("     Ver 0.1     ")
-				print("   OS Created.   ")
+				print("     Ver 0.2     ")
+				print("Added Verboseness")
+				print("and rewrote comm-")
+				print("   and system.   ")
 				print("=================")
+				ask=input("Show more? 1:Yes 2:No >")
+				if ask==1:
+					print("     Ver 0.1     ")
+					print("   OS Created.   ")
+					print("=================")
+				elif ask==2:
+					exit
+				else:
+					print("Error: Invalid answer")
 			elif ask==2:
 				exit
 			else:
 				print("Error: Invalid answer")
-		elif ask==2:
-			exit
-		else:
-			print("Error: Invalid answer")
 	elif command == 10:
 		sys.exit("exit::user requested exit")
+	elif command == 11:
+		print "Built", bd # app name: buildtime
 	else:
 		print("Invalid command.") # only applies for numbers; text causes a crash
