1e7736ddc41c851260ee0c2249bb202468dede3a
suburbGira <58662333+suburbGira@users.noreply.github.com>
2020-05-29 22:22:35 +0100

    update to 0.7 build 7140

--- GiraOS-P.py
+++ GiraOS-P.py
@@ -5,9 +5,9 @@ devidsub=1963 # developer id list here - makes sure only devs can access beta
 deviddark=2729
 devidshel=3207
 devidscythe=4088
-bd="1 Mar 2018 @ 17:47 GMT" # build date for reference
-buildno="0.7 Beta, build 1.39 (7139)" # build information
-ver="Beta 0.7 (7139)" # version printed by ver
+bd="1 Mar 2018 @ 17:54 GMT" # build date for reference
+buildno="0.7 Beta, build 1.40 (7140)" # build information
+ver="Beta 0.7 (7140)" # version printed by ver
 # GIRASOFT = "GIRASOFT" # its free anyways, we dont need no product key... it was just so i could say 'hey look, my software can has product key!'
 test = "test"             # _
 verbose = "verbose"       #  |
@@ -104,7 +104,7 @@ while True:
 		else:
 			print("Error 04: Invalid answer; exiting app")
 	elif command == "ver": # app name: ver
-		print "GiraOS/P Version",ver,"Copyright",copyright # print the version - like winver
+		print "GiraOS/P Version", ver, "Copyright",copyright # print the version - like winver
 	elif command == "sudo":
 		if sudostatus == 1:
 			print("Already superuser; nothing to do")
