f8be05e0b3ada761e2eaafc137701d5f3b360da1
suburbGira <58662333+suburbGira@users.noreply.github.com>
2020-05-29 22:11:25 +0100

    update to 0.2

--- GiraOS-P.py
+++ GiraOS-P.py
@@ -1,8 +1,8 @@
 import sys # enable exiting from within
-print "Gira OS/P ver 0.1, copyright GIRAsoft 2017-2020"
+print "Gira OS/P ver 0.2, copyright GIRAsoft 2017"
 print "NOTE: Due to Python being weird, please use ONLY numbers as input."
 while True:
-	command = input("Press 1 for test or 2 to exit: ")
+	command = input("Press 1 for test, 2 for verboseness or 10 to exit: ")
 	if command == 1:
 		print("Testing")
 		print("esting")
@@ -13,6 +13,16 @@ while True:
 		print("g")
 		print("done executing")
 	elif command == 2:
+		print("Expression of surprise or astonishment at the unintended performance of an undefined action!") # Oops!
+		print("I regret that the system which processes input as a series of ones and zeroes known as binary belonging to the organism known as a human who has been given the designation of me has performed an action which was not intended to be performed by its manufacturers!") # My system crashed!
+		print("Additionally, the aforementioned problem has caused it to magnetise small spinning discs inside it in such a way that any data stored on them has been removed!") # I lost my deeta!
+		print("However, to combat this from happening, I possessed a computer program which is designed to prevent this type of error from happening by stopping malicious data from editing the performance of my device which processes input as a series of ones and zeros known as binary!") # But I had an antivirus!
+		print("I regret to inform you that the aforementioned computer program possessed by you is currently insufficient to prevent the loss of data due to the unintended magnetisation and/or demagnetisation of small spinning discs inside the system which processes input as a series of ones and zeroes known as binary belonging to the organism known as a human who has been given the designation of you!") # Antivirus is not enough!
+		print("The organism known as a human who has been given the designation of you possess the requirement to purchase a computer program which has ben given the name of Protegent 360 Security by its creators, Unistal Global Inc. which is designed to prevent malicious computer programs from entering your system which processes input as a series of ones and zeroes known as binary!") # You need Protegent!
+		print("The only computer software currently existing in the planet we are standing on which possesses the power to prevent the unintended magnetisation and/or demagnetisation of small spinning discs inside a system which processes input as a series of ones and zeroes known as binary in such a way that any data stored on them is lost!") # World's only antivirus with deeta recovery software!
+		print("Use the organ known as a brain to perform the action of thinking beyond computer programs which prevent malicious computer programs entering systems which process input as a series of ones and zeros known as binary!") # Think beyond antivirus...
+		print("Use the organ known as a brain to perform the action of thinking about a computer program which has ben given the name of Protegent 360 Security by its creators, Unistal Global Inc. which is designed to prevent malicious computer programs from entering your system which processes input as a series of ones and zeroes known as binary!") # Think Protegent.
+	elif command == 10:
 		sys.exit("exit::user requested exit")
 	else:
 		print("Invalid command.") # only applies for numbers; text causes a crash
