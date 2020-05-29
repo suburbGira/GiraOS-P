f49222f37df5e853d490e9a4eef77fd44357339e
suburbGira <58662333+suburbGira@users.noreply.github.com>
2020-05-29 22:11:43 +0100

    update to 0.3

--- GiraOS-P.py
+++ GiraOS-P.py
@@ -1,8 +1,17 @@
+#GGGGGGGGGGG IIIIIIIII RRRRRRRR AA                                       22222   00000   11   77777
+#G               I     R      R A A        sssss   ooooo   ffff    t    2     2 0    00 1 1       7
+#G               I     R      R A  A      s     s o     o f    f ttttt       2  0   0 0   1      7
+#G               I     RRRRRRRR A   A     s       o     o f        t        2   0  0  0   1     7
+#G   GGGGGGG     I     R R      AA   A     sssss  o     o ffff     t       2    0  0  0   1    7
+#G         G     I     R  R     A AA  A         s o     o f        t      2     0 0   0   1   7
+#G         G     I     R   R    A   AA A  s     s o     o f        t     2      00    0   1   7
+#GGGGGGGGGGG IIIIIIIII R    R   A     AAA  sssss   ooooo  f        t    2222222  00000  11111 7     DO NOT SELL THIS SOFTWARE
 import sys # enable exiting from within
-print "Gira OS/P ver 0.2, copyright GIRAsoft 2017"
-print "NOTE: Due to Python being weird, please use ONLY numbers as input."
+print("GiraOS/P ver 0.3 -- copyright GIRAsoft 2017. You should have recieved a copy of the GNU GPL with this software. If not, you can find it on the Internet, or in most Linux software.")
+print("DO NOT ATTEMPT TO SELL THIS SOFTWARE. IF YOU HAVE PAID FOR THIS SOFTWARE, PLEASE CONTACT greatgiratheepic@gmail.com!")
+print("NOTE: Due to Python being weird, please use ONLY numbers as input.")
 while True:
-	command = input("Press 1 for test, 2 for verboseness or 10 to exit: ")
+	command = input("Press 1 for test, 2 for verboseness, 3 to print the changelog or 10 to exit: ")
 	if command == 1:
 		print("Testing")
 		print("esting")
@@ -22,6 +31,30 @@ while True:
 		print("The only computer software currently existing in the planet we are standing on which possesses the power to prevent the unintended magnetisation and/or demagnetisation of small spinning discs inside a system which processes input as a series of ones and zeroes known as binary in such a way that any data stored on them is lost!") # World's only antivirus with deeta recovery software!
 		print("Use the organ known as a brain to perform the action of thinking beyond computer programs which prevent malicious computer programs entering systems which process input as a series of ones and zeros known as binary!") # Think beyond antivirus...
 		print("Use the organ known as a brain to perform the action of thinking about a computer program which has ben given the name of Protegent 360 Security by its creators, Unistal Global Inc. which is designed to prevent malicious computer programs from entering your system which processes input as a series of ones and zeroes known as binary!") # Think Protegent.
+	elif command == 3:
+		print("     Ver 0.3      ")
+		print("Added logo in code")
+		print("==================")
+		ask=input("Show more? 1:Yes 2:No >")
+		if ask==1:
+			print("     Ver 0.2     ")
+			print("Added Verboseness")
+			print("and rewrote comm-")
+			print("   and system.   ")
+			print("=================")
+			ask=input("Show more? 1:Yes 2:No >")
+			if ask==1:
+				print("     Ver 0.1     ")
+				print("   OS Created.   ")
+				print("=================")
+			elif ask==2:
+				exit
+			else:
+				print("Error: Invalid answer")
+		elif ask==2:
+			exit
+		else:
+			print("Error: Invalid answer")
 	elif command == 10:
 		sys.exit("exit::user requested exit")
 	else:
